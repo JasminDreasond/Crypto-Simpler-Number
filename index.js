761c166fd9740575901dbebc82720f509712cf86
Yasmin Seidel <tiny@puddy.club>
2022-10-07 23:47:06 -0300

    first prepare

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = function(number, point) {
+
+    // Get Value
+    const tinyNumber = Number(new BigNumber(number).toFixed());
+
+    // Complete
+    return tinyNumber;
+
+};
---
+++ test.js
@@ -0,0 +1,8 @@
+// Module
+const cryptoNumber = require('./index');
+
+// Get Number
+const result = cryptoNumber('1000000000000000000', 6);
+
+// Result
+console.log(result);
